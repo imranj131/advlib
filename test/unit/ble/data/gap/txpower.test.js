12f8186dcf606caade2722150e4ff8a1abde3347
imranj131 <imranj131@yahoo.com>
2015-06-11 13:26:30 -0400

    adding tests

--- test/unit/ble/data/gap/txpower.test.js
+++ test/unit/ble/data/gap/txpower.test.js
@@ -3,7 +3,7 @@
  * We believe in an open Internet of Things
  */
 
-var localname = require("../../../../../lib/ble/data/gap/txpower.js");
+var txpower = require("../../../../../lib/ble/data/gap/txpower.js").process;
 var assert = require ('assert'); 
 
 // Constants for the scenario
@@ -11,16 +11,16 @@ var CURSOR = 0;
 var ADVERTISER_DATA = {};
 
 // Inputs for the scenario
-var INPUT_DATA = '';
+var INPUT_DATA = 'ba50e1fe0201050c097265656c794163746976650100';
 
 // Expected outputs for the scenario
-var EXPECTED_DATA = '';
+var EXPECTED_DATA = '-31dBm';
 
 describe('ble data txpower', function() {
 
   // Test the process function
-  it('should parse BLE advertiser data service data', function() {
-  	process(INPUT_DATA_APPLE, CURSOR, ADVERTISER_DATA);
+  it('should parse BLE advertiser TX power', function() {
+  	txpower(INPUT_DATA, CURSOR, ADVERTISER_DATA);
     assert.deepEqual(ADVERTISER_DATA.txPower, EXPECTED_DATA);
   });
   
