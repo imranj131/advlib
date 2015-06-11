3dd5a056051a010cbd2a401a7fa9c1a2c760eeb4
imranj131 <imranj131@yahoo.com>
2015-06-11 13:05:33 -0400

    need to figure out input and output

--- test/unit/ble/data/gap/localname.test.js
+++ test/unit/ble/data/gap/localname.test.js
@@ -11,22 +11,23 @@ var CURSOR = 0;
 var ADVERTISER_DATA = {};
 
 // Inputs for the scenario
-var INPUT_DATA = '020109';
+var INPUT_DATA_SHORTENED_LOCAL_NAME = '23084c000215b9407f30f5f8466eaff925556b57fe6d294c903974';
+var INPUT_DATA_COMPLETE_LOCAL_NAME = '23094c000215b9407f30f5f8466eaff925556b57fe6d294c903974'
 
 // Expected outputs for the scenario
-var EXPECTED_DATA_SHORTENED_LOCAL_NAME ='\u0001';
+var EXPECTED_DATA_SHORTENED_LOCAL_NAME ='';
 var EXPECTED_DATA_COMPLETE_LOCAL_NAME ='reelyActive';
 
 describe('ble data localname', function() {
 
   // Test the process function
   it('should convert ble advertiser data to a shortened local name', function() {
-  	localname.shortenedLocalName(INPUT_DATA, CURSOR, ADVERTISER_DATA);
-    assert.deepEqual(ADVERTISER_DATA.shortenedLocalName[0], EXPECTED_DATA_SHORTENED_LOCAL_NAME);
+  	localname.shortenedLocalName(INPUT_DATA_SHORTENED_LOCAL_NAME, CURSOR, ADVERTISER_DATA);
+    assert.deepEqual(ADVERTISER_DATA.shortenedLocalName, EXPECTED_DATA_SHORTENED_LOCAL_NAME);
    
   });
   it('should convert ble advertiser data to a complete local name', function() {
-  	localname.completeLocalName(INPUT_DATA, CURSOR, ADVERTISER_DATA);
+  	localname.completeLocalName(INPUT_DATA_COMPLETE_LOCAL_NAME, CURSOR, ADVERTISER_DATA);
     assert.deepEqual(ADVERTISER_DATA.completeLocalName, EXPECTED_DATA_COMPLETE_LOCAL_NAME);
   });
 
