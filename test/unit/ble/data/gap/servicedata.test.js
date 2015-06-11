3d67daa7114a79415b070e11ead2eeb8c11c1398
imranj131 <imranj131@yahoo.com>
2015-06-11 13:20:29 -0400

    adding tests

--- test/unit/ble/data/gap/servicedata.test.js
+++ test/unit/ble/data/gap/servicedata.test.js
@@ -3,7 +3,7 @@
  * We believe in an open Internet of Things
  */
 
-var localname = require("../../../../../lib/ble/data/gap/servicedata.js");
+var servicedata = require("../../../../../lib/ble/data/gap/servicedata.js").process;
 var assert = require ('assert'); 
 
 // Constants for the scenario
@@ -11,12 +11,12 @@ var CURSOR = 0;
 var ADVERTISER_DATA = {};
 
 // Inputs for the scenario
-var INPUT_DATA = '';
+var INPUT_DATA = '421655daba50e1fe0201050c097265656c794163746976650100';
 
 // Expected outputs for the scenario
 var EXPECTED_DATA ={
-	uuid:'';,
-	data:'';
+	uuid:"da55",
+	data:"ba50e1fe0201050c097265656c794163746976650100",
 };
 
 
@@ -24,7 +24,7 @@ describe('ble data servicedata', function() {
 
   // Test the process function
   it('should parse BLE advertiser data service data', function() {
-  	process(INPUT_DATA_APPLE, CURSOR, ADVERTISER_DATA);
+  	servicedata(INPUT_DATA, CURSOR, ADVERTISER_DATA);
     assert.deepEqual(ADVERTISER_DATA.serviceData, EXPECTED_DATA);
   });
   
