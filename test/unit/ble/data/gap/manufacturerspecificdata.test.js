391673ebcd4cb4d7031c98ae2e9348533a1be154
imranj131 <imranj131@yahoo.com>
2015-06-11 21:39:57 -0400

    adding tests

--- test/unit/ble/data/gap/manufacturerspecificdata.test.js
+++ test/unit/ble/data/gap/manufacturerspecificdata.test.js
@@ -8,7 +8,7 @@ Bluetooth Core Specification:
 0xFF	«Manufacturer Specific Data»
 */
 
-var localname = require("../../../../../lib/ble/data/gap/manufacturerspecificdata.js").process;
+var manufacturerspecificdata = require("../../../../../lib/ble/data/gap/manufacturerspecificdata.js").process;
 var assert = require ('assert'); 
 
 // Constants for the scenario
@@ -25,19 +25,19 @@ var EXPECTED_DATA_APPLE_AND_IBEACON = {
 																		uuid: "b9407f30f5f8466eaff925556b57fe6d",
 														        major: "294c",
 														        minor: "9039",
-														        txPower: "-74dBm"
+														        txPower: "116dBm"
 };
 
 describe('ble data manufacturerspecificdata', function() {
 
   // Test the process function
-  it('should convert ble advertiser data to a apple manufacturer specificdata', function() {
-  	localname(INPUT_DATA_COMPANY_ONLY, CURSOR, ADVERTISER_DATA);
+  it('should convert ble advertiser data to a apple manufacturer specific data', function() {
+  	manufacturerspecificdata(INPUT_DATA_COMPANY_ONLY, CURSOR, ADVERTISER_DATA);
     assert.deepEqual(ADVERTISER_DATA.manufacturerSpecificData.companyIdentifierCode, EXPECTED_DATA_COMPANY_ONLY);
   });
   it('should convert ble advertiser data to apple and iBeacon manufacturer specificdata', function() {
   	ADVERTISER_DATA.manufacturerSpecificData.iBeacon = {};
-  	localname(INPUT_DATA_IBEACON, CURSOR, ADVERTISER_DATA);
+  	manufacturerspecificdata(INPUT_DATA_IBEACON, CURSOR, ADVERTISER_DATA);
     assert.deepEqual(ADVERTISER_DATA.manufacturerSpecificData.iBeacon, EXPECTED_DATA_APPLE_AND_IBEACON);
   });
 });
