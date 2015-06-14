9fca4b37175b32f03c0e038aacc140f56993ea8f
imranj131 <imranj131@yahoo.com>
2015-06-14 17:30:29 -0400

    adding tests

--- test/unit/ble/data/gap/manufacturerspecificdata.test.js
+++ test/unit/ble/data/gap/manufacturerspecificdata.test.js
@@ -20,7 +20,10 @@ var INPUT_DATA_COMPANY_ONLY = '03ff4c00';
 var INPUT_DATA_IBEACON = '23ff4c000215b9407f30f5f8466eaff925556b57fe6d294c903974';
 
 // Expected outputs for the scenario
-var EXPECTED_DATA_COMPANY_ONLY ='004c';
+var EXPECTED_DATA_COMPANY_ONLY = {
+                               companyIdentifierCode: "004c",
+                               data: ""
+};
 var EXPECTED_DATA_APPLE_AND_IBEACON = {
 																		uuid: "b9407f30f5f8466eaff925556b57fe6d",
 														        major: "294c",
@@ -33,7 +36,7 @@ describe('ble data manufacturerspecificdata', function() {
   // Test the process function
   it('should convert ble advertiser data to a apple manufacturer specific data', function() {
   	manufacturerspecificdata(INPUT_DATA_COMPANY_ONLY, CURSOR, ADVERTISER_DATA);
-    assert.deepEqual(ADVERTISER_DATA.manufacturerSpecificData.companyIdentifierCode, EXPECTED_DATA_COMPANY_ONLY);
+    assert.deepEqual(ADVERTISER_DATA.manufacturerSpecificData, EXPECTED_DATA_COMPANY_ONLY);
   });
   it('should convert ble advertiser data to apple and iBeacon manufacturer specificdata', function() {
   	ADVERTISER_DATA.manufacturerSpecificData.iBeacon = {};
